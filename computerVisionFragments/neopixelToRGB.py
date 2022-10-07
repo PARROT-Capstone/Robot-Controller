6eb71a96f2ed224e1802832fe499adb43f7b59ed
Saral Tayal <saraltayal@Sarals-MacBook-Air.local>
2022-10-07 10:13:06 -0400

    WIP dynamic masking

---
+++ computerVisionFragments/neopixelToRGB.py
@@ -0,0 +1,73 @@
+'''
+This module is copy-pasted from the following link https://www.folkstalk.com/2022/09/getting-pixels-with-mouse-click-with-code-examples.html
+The only change is reading an image from a camera instead of a file and printing the RGB value of the pixels
+'''
+# importing the module
+import cv2
+import numpy as np
+
+# function to display the coordinates of
+# of the points clicked on the image
+def click_event(event, x, y, flags, params):
+ 
+    # checking for left mouse clicks
+    if event == cv2.EVENT_LBUTTONDOWN:
+ 
+        # displaying the coordinates
+        # on the Shell
+        print(x, ' ', y)
+ 
+        # displaying the coordinates
+        # on the image window
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, str(x) + ',' +
+                    str(y), (x,y), font,
+                    1, (255, 0, 0), 2)
+        cv2.imshow('image', img)
+ 
+    # checking for right mouse clicks    
+    if event==cv2.EVENT_RBUTTONDOWN:
+ 
+        # displaying the coordinates
+        # on the Shell
+        print(x, ' ', y)
+
+        # print the RGB value of the pixel
+        print(img[y, x]) #(B, G, R)
+ 
+        # displaying the coordinates
+        # on the image window
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        b = img[y, x, 0]
+        g = img[y, x, 1]
+        r = img[y, x, 2]
+        cv2.putText(img, str(b) + ',' +
+                    str(g) + ',' + str(r),
+                    (x,y), font, 1,
+                    (255, 255, 0), 2)
+        cv2.imshow('image', img)
+ 
+# driver function
+if __name__=="__main__":
+ 
+    while(True):
+        # reading the image
+        cap = cv2.VideoCapture(1)
+        _, img = cap.read()
+        print(np.shape(img))
+        while(np.shape(img)[0] < 100):
+            _, img = cap.read()
+        print(np.shape(img))
+
+        # displaying the image
+        cv2.imshow('image', img)
+    
+        # setting mouse handler for the image
+        # and calling the click_event() function
+        cv2.setMouseCallback('image', click_event)
+    
+        # wait for a key to be pressed to exit
+        cv2.waitKey(0)
+    
+        # close the window
+        cv2.destroyAllWindows()
