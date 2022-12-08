847bf11c0d871a1c0e05e7a6d8b908ddb15fd4ff
Omkar Savkur <osavkur@andrew.cmu.edu>
2022-12-08 15:27:18 -0500

    Electromagnet tests

--- comms_test.py
+++ comms_test.py
@@ -1,42 +1,23 @@
 import requests
 import time
 
+delay = 0.1 # sec
+
 # main functino for testing the comms module
 def main():
     print("Testing comms module")
-    robot_url = "http://172.26.171.154"
-    
-    # get user input for rgb values
-    r = input("Enter red value: ")
-    g = input("Enter green value: ")
-    b = input("Enter blue value: ")
-    
-    # loop through index 0 to 11
-    for i in range(12):
-        json = {"dtype": "rgbi", 
-                "r": r,
-                "g":g,
-                "b": b,
-                "index": str(i)
+    robot_url = "http://parrot-robot1.wifi.local.cmu.edu"
+
+    for i in range(1,11):
+        json = {"dtype": "pallet", 
+                "power": i % 2
                 }
         x = requests.post(robot_url, data=json)
-        time.sleep(0.5)
-    
-    # turn off the lights
-    for i in range(12):
-        json = {"dtype": "rgbi",
-                "r": "0",
-                "g":"0",
-                "b": "0",
-                "index": str(i)
-                }
-        x = requests.post(robot_url, data=json)
-        time.sleep(0.5)
+        time.sleep(delay)
     
     return 0    
     
 
 # call main function on start
 if __name__ == '__main__':
-    while True:
-        main()
+    main()
